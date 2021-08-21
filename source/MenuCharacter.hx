3ba0fff412d5cff49133fa03653305d1264b7d9e
Clowfoe <70791621+Clowfoe@users.noreply.github.com>
2021-08-21 03:54:27 -0400

    Revert "ss"
    This reverts commit fbc332bfdfd0b21f6b37f78355964fd0ee872406, reversing
    changes made to 17cd3431fdc305b4b583a53a7039022f0e49e89f.

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -30,8 +30,7 @@ class MenuCharacter extends FlxSprite
 		'pico' => new CharacterSetting(0, 0, 1.0, true),
 		'mom' => new CharacterSetting(-30, 140, 0.85),
 		'parents-christmas' => new CharacterSetting(100, 130, 1.8),
-		'senpai' => new CharacterSetting(-40, -45, 1.4),
-		'perry' => new CharacterSetting(-40, -5, 1.7)
+		'senpai' => new CharacterSetting(-40, -45, 1.4)
 	];
 
 	private var flipped:Bool = false;
@@ -60,7 +59,6 @@ class MenuCharacter extends FlxSprite
 		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24, false);
 		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24, false);
 		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24, false);
-		animation.addByPrefix('perry', "Perry Idle", 24, false);
 
 		setGraphicSize(Std.int(width * scale));
 		updateHitbox();
